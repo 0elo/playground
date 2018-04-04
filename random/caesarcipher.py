f93753db2aced29b7068881befe6bce2c344020c
Kevin Yu <kevyu1@umbc.edu>
2018-04-03 22:03:53 -0400

    Amend broken no-arg functionality and spruce up comments

--- random/caesarcipher.py
+++ random/caesarcipher.py
@@ -32,7 +32,7 @@ class Cipher:
 
     '''
     
-    def __init__(self, string, shift = None):
+    def __init__(self, string: str, shift: int = 0):
         '''This is the __init__ method.
 
         Args:
@@ -41,31 +41,60 @@ class Cipher:
 
         '''
         self.string = string
-        if shift == None:
-            self.shift = 0
-        else:
-            self.shift = shift
+        self.shift = shift
         self.encoded = ''
         self.decoded = ''
 
     def printEncodedHash(self):
-        print(self.encodeHashTable())
+        '''Prints the encoded string using hash table implementation.
+
+        '''
+
+        # If this is an empty string, run encodeHashTable()
+        # to set encoded string to self.encoded
+        if not self.encoded:
+            self.encodeHashTable()
+
+        print(self.encoded)
+
         
     def printDecodedHash(self):
-        print(self.decodeHashTable())
+        '''Prints the decoded string using hash table implementation.
 
-    def cleanString(self, string):
+        '''
+        
+        # If this is an empty string, run decodeHashTable()
+        # to set decoded string to self.decoded
+        if not self.decoded:
+            self.decodeHashTable()
+            
+        print(self.decoded)
+
+    def cleanString(self, string: str) -> str:
+        '''Clears input string of non alphabetical characters
+
+        Args:
+            string (str): The string to be cleaned
+
+        '''
         cleaned = ''.join([i for i in string.lower() if i.isalpha()])
+
         return cleaned
 
-    def detectEnglish(self, string):
-        '''
+    def detectEnglish(self, string: str) -> bool:
+        '''Detects if a string is English by checking
+        against polyglot and langdetect libraries.
+
         Current solution to detect if string is English
         is to check against both langdetect and polyglot.
         Longer sentences are split by every ten words and
         verified via polyglot a second time.
-        Will make solution more robust (if necessary)
-        following extensive testing.
+
+        Will make solution more robust following extensive testing.
+
+        Args:
+            string (str): The string to be analyzed
+        
         '''
         text = Text(string)
         if detect(string) == 'en' and text.language.code == 'en':
@@ -96,6 +125,9 @@ class Cipher:
         return False
 
     def printPrediction(self):
+        '''Prints prediction of the shift and decoded string.
+
+        '''
         guess, original, decoded = self.detectByFrequency()
         
         print('\nOriginal string:\n{}'.format(original))
@@ -103,7 +135,12 @@ class Cipher:
         print('\nDecoded string:\n{}\n'.format(decoded))
     
     def detectByFrequency(self):
+        '''Detects whether a string is English via frequency analysis.
 
+        '''
+
+        # If the string is already English, just
+        # return the string
         if self.detectEnglish(self.string):
             return(0,self.string,self.string)
         #temp = self.string
@@ -143,7 +180,7 @@ class Cipher:
         encodedValues = ALPHABET[self.shift:] + ALPHABET[:self.shift]
         encodeMap = dict(zip(ALPHABET, encodedValues))
         self.encoded = ''.join([encodeMap[c] if c.islower() else encodeMap[c.lower()].upper() if c.lower() in encodeMap else c for c in self.string])
-        return('Encoded string:\n{}\n'.format(self.encoded))
+        #return('Encoded string:\n{}\n'.format(self.encoded))
 
     def decodeHashTable(self):
         '''Decodes message using a hash table implementation.
@@ -155,7 +192,7 @@ class Cipher:
         decodedValues = ALPHABET[-self.shift:] + ALPHABET[:-self.shift]
         decodeMap = dict(zip(ALPHABET, decodedValues))
         self.decoded = ''.join([decodeMap[c] if c.islower() else decodeMap[c.lower()].upper() if c.lower() in decodeMap else c for c in self.string])
-        return('Decoded string:\n{}\n'.format(self.decoded))
+        #return('Decoded string:\n{}\n'.format(self.decoded))
     
     def encodeOrdinal(self):
         '''Encodes message using ordinals, with the ugliest possible list comprehension.
@@ -179,6 +216,8 @@ class Cipher:
 class handleArgs:
 
     def __init__(self):
+        # This is the init method.
+
         self.encodeFlag = False
         self.decodeFlag = False
         self.handleDetect = False
@@ -189,20 +228,25 @@ class handleArgs:
         self.conversion = ''
             
     def getShift(self):
+        # Accessor for shift
         return self.shift
 
     def getString(self):
+        # Accessor for input string
         return self.string
 
     def printString(self):
         return 'Original message: {}'.format(self.string)
         
     def getArgs(self):
-        
+
+        # Top level arg handler with program description
         parser = argparse.ArgumentParser(description='This is a multifunctional Caesar cipher tool written in Python 3.6. The detect command takes an string encrypted by a Caesar cipher with an unknown shift, and makes a prediction of the how the string was encoded. The file and string commands call functionality to encode or decode a string by a user-specified shift. The string can be read in from the command line or a file.')
 
-        subparser = parser.add_subparsers(dest='selection', help = 'commands')
+        # Add subparsers for the Detect, File, and Text commands
+        subparser = parser.add_subparsers(dest='selection', help = 'Select from {detect, file, text}')
         subparser.required = False
+        
         # Detect Command
         detect_command = subparser.add_parser('detect', help='Detect shift of an encoded string')
         detect_command.add_argument('d_file', action='store', help='File to be decoded')
@@ -218,12 +262,16 @@ class handleArgs:
         text_command.add_argument('t_conversion', action='store', help="Choose to 'encode' or 'decode' the string", type=str)
         text_command.add_argument('t_shift', action='store', help='Shift the string by this amount', type=int)
         text_command.add_argument('thetext', action='store', help='Text to be encoded or decoded', type=str)
-        
+
+        # Stores args into dict
         args = vars(parser.parse_args())
-        #print(args)
-        
+
+        # Set appropriate values if Detect command selected
         if args['selection'] == 'detect':
+
             self.handleDetect = True
+
+            # Parse file and set as input string
             if Path(args['d_file']).is_file():
                 theFile = open(Path(__location__ / args['d_file']), 'r')
                 self.string = ''.join([line for line in theFile])
@@ -231,19 +279,26 @@ class handleArgs:
 
             else:
                 parser.error('Could not locate file!')
+                
             self.conversion = 'decode'
-            
+
+        # Set appropriate values if File command selected    
         elif args['selection'] == 'file':
+            
             self.conversion = args['f_conversion']
             self.handleFile = True
             self.shift = args['f_shift']
+
+            # Parse file and set as input string
             if Path(args['thefile']).is_file():
                 theFile = open(Path(__location__ / args['thefile']), 'r')
                 self.string = ''.join([line for line in theFile])
                 theFile.close()
+                
             else:
                 parser.error('Could not locate file!')
 
+        # Set appropriate values if Text command selected
         elif args['selection'] == 'text':
             self.conversion = args['t_conversion']
             self.handleText = True
@@ -251,37 +306,73 @@ class handleArgs:
             self.string = args['thetext']
         
         else:
+            ''' Runs if no command line args are provided.
+
+            Prompts user for input words, the option to encode or decode,
+            and the value to shfit by. The resulting encoded/decoded string
+            is printed.
             '''
-            This statement never runs. Need to check how to make args optional
-            '''
-            self.string = input('Please enter a sentence: ')
-            conversionType = input('Encode or decode: ')
-            while conversionType != 'encode' or conversionType != 'decode':
-                conversionType = input('Encode or decode: ')
+
+            # Get what the user wants
+            self.string = input('Please enter a sentence: ').strip()
+
+            # Get encode or decode
+            conversionType = input('Enter (encode or decode): ').strip()
+
+            # Validate that encode or decode was provided as input
+            while conversionType != 'encode' and conversionType != 'decode':
+
+                conversionType = input('Enter (encode or decode): ').strip()
+
+            # Assign conversion type
             self.conversion = conversionType
-            shiftNum = input('Please enter a shift amount: ')
-            while not shiftNum.isdigit():
-                shiftNum = input('Please enter a shift amount: ')
+
+            # Validate that input shift is an int
+            while True:
+                shiftNum = input('Please enter a shift amount: ').strip()
+
+                try:
+                    
+                    shiftNum = int(shiftNum)
+                    break;
+                    
+                except ValueError:
+                    print('This is not a number')
+
+            # Assign shift
             self.shift = shiftNum
 
+        # Set encode and decode flags
         if self.conversion == 'encode':
+            
             self.encodeFlag = True
+
         elif self.conversion =='decode':
+
             self.decodeFlag = True
+
         else:
+
+            # Error checking if encoding/decoding not specified as cmd line arg
             parser.error("Enter 'encode' or 'decode' for the conversion argument!")
 
+        # If the shift is greater than the length of the alphabet,
+        # that's the same as the modulus of the shift and alphabet length.
         if ALPHABET_LEN < self.shift:
+
             self.shift = self.shift % ALPHABET_LEN
 
             
 if __name__ == '__main__':
+
+    # Masthead
     print('\n{}\n        Z E R O E L O \' S\n    C A E S A R   C I P H E R\n{}\n'.format('*'*36,'*'*36))
     
     args = handleArgs()
     args.getArgs()
-    message = Cipher(args.getString(), args.getShift())
     
+    message = Cipher(args.getString(), args.getShift())
+
     if args.handleDetect:
         message.printPrediction()
     else:
